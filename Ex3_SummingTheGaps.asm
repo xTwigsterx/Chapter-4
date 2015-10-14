f39d1596b642f03e5e459282b2421269f6a1e89e
Zachariah Alber <zachariah.man.of.god@gmail.com>
2015-10-14 18:33:25 -0400

    assignment done
    made the appropriate changes

--- Ex3_SummingTheGaps.asm
+++ Ex3_SummingTheGaps.asm
@@ -8,11 +8,21 @@ ExitProcess proto,dwExitCode:dword
 .data
 dwarray dword  0,2,5,9,10
 count EQU (LENGTHOF dwarray)
-
+total dword 0
 .code
 main proc
 	
-	;Your code goes here
+	mov,esi OFFSET dwarray
+	
+	mov	ecx,count
+
+L1:
+	mov	eax,[esi]		; move to eax 
+	inc	esi			; increment
+	mov	ebx,[esi]		; move to ebx
+	sub	ebx,eax		; difference
+	add	total,ebx		; adds the two values
+	Loop L1
 	
 	invoke ExitProcess,0
 main endp
